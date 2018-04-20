5af58367e4dc3602bae4614c96b50ecfe33af106
Tien Pham <r3dsky02@yahoo.com.vn>
2018-04-20 20:57:31 +0300

    Rect

--- pages/bitcoin.js
+++ pages/bitcoin.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React from "react";
+
+function Background({width, height}) {
+  return (
+    <svg width={width} height={height}>
+      <rect width={width} height={height} fill="steelblue" />
+    </svg>
+  );
+}
 
 class App extends React.Component {
   state = {
     data: {}
-  }
+  };
 
-  render(){
-    const { data } = this.state
-    return <div>App</div>
+  render() {
+    const { data } = this.state;
+    return (
+      <div>
+        <Background width={100} height={100} />
+        <style jsx>{`
+          .app {
+            display: flex;
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+          }
+        `}</style>
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
