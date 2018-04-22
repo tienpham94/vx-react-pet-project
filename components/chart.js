934cc913a59dfbd7a1e7bc7a611944e6651d506f
Tien Pham <r3dsky02@yahoo.com.vn>
2018-04-22 21:18:23 +0300

    Working LinePath

--- components/chart.js
+++ components/chart.js
@@ -1,20 +1,38 @@
 import { withParentSize } from "@vx/responsive";
+import { scaleTime, scaleLinear } from "@vx/scale";
+import { linePath } from "@vx/shape";
+import LinePath from "@vx/shape/build/shapes/LinePath";
 
-function Chart({ parentWidth, parentHeight }) {
+function Chart({ data, parentWidth, parentHeight }) {
   const margin = {
     top: 15,
     bottom: 40,
     left: 0,
     right: 0
-  }
+  };
 
   const width = parentWidth - margin.left - margin.right;
   const height = parentHeight - margin.top - margin.bottom;
 
+  const x = d => new Date(d.time);
+  const y = d => d.price;
+  const minPrice = Math.min(...data.map(y));
+  const maxPrice = Math.max(...data.map(y));
+
+  const xScale = scaleTime({
+    range: [0, width],
+    domain: [Math.min(...data.map(x)), Math.max(...data.map(x))]
+  });
+
+  const yScale = scaleLinear({
+    range: [height, 0],
+    domain: [minPrice, maxPrice]
+  });
+
   return (
     <div>
-      <svg width={parentWidth} height={parentHeight}>
-        <rect width={parentWidth} height={parentHeight} fill="steelblue" />
+      <svg width={width} height={height}>
+        <LinePath data={data} x={x} y={y} yScale={yScale} xScale={xScale} />
       </svg>
     </div>
   );
